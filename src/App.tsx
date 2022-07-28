7f1f2642899e3dd1c247e4524d33327b55173ed2
VadimDev19 <vadimtkacenko066@gmail.com>
2022-07-28 22:17:30 +0300

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  console.log("app");
-  window.addEventListener("beforeinstallprompt", (ev) => {
-    console.log("beforeinstallprompt index", ev);
-  });
+  useEffect(() => {
+    console.log("effect");
+    window.addEventListener("beforeinstallprompt", (ev) => {
+      console.log("beforeinstallprompt app", ev);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button>clike me</button>
+        <button>clike me 2</button>
       </header>
     </div>
   );
